0b0a8668f689147e6fb1ccc0c7eb2d3ed42dab6e
Claudio <claudio@celk.com.br>
2015-07-20 16:43:06 -0300

    plugins

--- www/js/app.js
+++ www/js/app.js
@@ -47,6 +47,24 @@ app.run(function ($ionicPlatform, DB, FS, $rootScope, $ionicConfig, $cordovaDevi
                 }, function (err) {
                     console.log('GPS erro: (' + err.code + ') ' + err.message);
                 });
+                
+                function checkConnection() {
+                    var networkState = navigator.connection.type;
+
+                    var states = {};
+                    states[Connection.UNKNOWN]  = 'Unknown connection';
+                    states[Connection.ETHERNET] = 'Ethernet connection';
+                    states[Connection.WIFI]     = 'WiFi connection';
+                    states[Connection.CELL_2G]  = 'Cell 2G connection';
+                    states[Connection.CELL_3G]  = 'Cell 3G connection';
+                    states[Connection.CELL_4G]  = 'Cell 4G connection';
+                    states[Connection.CELL]     = 'Cell generic connection';
+                    states[Connection.NONE]     = 'No network connection';
+
+                    alert('#### CELK Connection type: ' + states[networkState]);
+                }
+
+                checkConnection();                
         });
             
         $cordovaFile.removeFile('celk/familias/familias.apk').then(function(){
